c536388c19539f0750ebaa5967aeb5b0bee57a23
Tymur Panzhyiev <t.panzhyiev@codiv.io>
2018-03-22 19:00:07 +0200

    Settings main screen bugs fixed V2

--- app/src/main/java/com/crypto/cryptoinfo/repository/network/ApiConstants.java
+++ app/src/main/java/com/crypto/cryptoinfo/repository/network/ApiConstants.java
@@ -5,7 +5,8 @@ public class ApiConstants {
     public static final String BASE_URL_COINMARKETCAP = "https://api.coinmarketcap.com/v1/";
     public static final String BASE_URL_GRAPHS = "https://graphs2.coinmarketcap.com/currencies/";
     public static final String BASE_URL_CRYPTOCOMPARE = "https://www.cryptocompare.com/api/";
-    public static final String COIN_SNAPSHOT = "data/coinsnapshot/";
+    public static final String BASE_URL_CRYPTOCOMPARE_NEW = "https://min-api.cryptocompare.com/";
+    public static final String COIN_SNAPSHOT = "data/top/exchanges/full";
 
     public static final String TICKERS = "ticker/?start=0&limit=2000";
     public static final String TICKER_SPECIFIC = "/ticker/{id}/";
--- app/src/main/java/com/crypto/cryptoinfo/repository/network/ModelImpl.java
+++ app/src/main/java/com/crypto/cryptoinfo/repository/network/ModelImpl.java
@@ -10,7 +10,7 @@ import retrofit2.Response;
 import rx.Observable;
 
 import static com.crypto.cryptoinfo.repository.network.ApiConstants.BASE_URL_COINMARKETCAP;
-import static com.crypto.cryptoinfo.repository.network.ApiConstants.BASE_URL_CRYPTOCOMPARE;
+import static com.crypto.cryptoinfo.repository.network.ApiConstants.BASE_URL_CRYPTOCOMPARE_NEW;
 import static com.crypto.cryptoinfo.repository.network.ApiConstants.BASE_URL_GRAPHS;
 
 public class ModelImpl {
@@ -24,6 +24,6 @@ public class ModelImpl {
     }
 
     public Observable<Response<JsonElement>> getCoinSnapshot(String fromSymbol, String toSymbol) {
-        return RestClient.getApiInterface(BASE_URL_CRYPTOCOMPARE).getCoinSnapshot(fromSymbol, toSymbol);
+        return RestClient.getApiInterface(BASE_URL_CRYPTOCOMPARE_NEW).getCoinSnapshot(fromSymbol, toSymbol);
     }
 }
