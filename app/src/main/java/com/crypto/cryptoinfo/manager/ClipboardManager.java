a5629b291825c90e134f00f395aa8b696f78b738
Tymur Panzhyiev <t.panzhyiev@codiv.io>
2018-04-12 10:41:23 +0300

    minor improvements

---
+++ app/src/main/java/com/crypto/cryptoinfo/manager/ClipboardManager.java
@@ -0,0 +1,25 @@
+package com.crypto.cryptoinfo.manager;
+
+
+import android.content.ClipData;
+import android.content.Context;
+
+public class ClipboardManager {
+
+    private Context mContext;
+
+    private android.content.ClipboardManager mClipboardManager;
+
+//    @Inject
+    public ClipboardManager(Context context){
+        mContext = context;
+        mClipboardManager = (android.content.ClipboardManager)
+                context.getSystemService(Context.CLIPBOARD_SERVICE);
+    }
+
+    public void copyToClipboard(String text){
+        ClipData clip = ClipData.newPlainText(mContext.getPackageName(), text);
+        mClipboardManager.setPrimaryClip(clip);
+    }
+
+}
